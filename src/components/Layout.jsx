4a6ad2342f1408053ad2f898c3a211893c231426
Sanders Gutiérrez <ing.sanders@gmail.com>
2023-05-05 13:00:58 -0300

    chore: run workflow when has been a release created (#2)
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

---
+++ src/components/Layout.jsx
@@ -0,0 +1,43 @@
+'use strict'
+
+import { Footer, Header, Main } from '@/components'
+import { ButtonGoToUp } from '@/components/ButtonGoToUp'
+import Head from 'next/head'
+import { useEffect, useState } from 'react'
+import { FaArrowUp } from 'react-icons/fa'
+
+export function Layout(props) {
+    const [active, setActive] = useState('')
+
+    useEffect(() => {
+        if (!(typeof window != 'undefined')) {
+            throw new Error('The object window is not defined')
+        }
+
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 100) {
+                setActive('active')
+            } else {
+                setActive('')
+            }
+        })
+    }, [])
+
+    return (
+        <>
+            <Head>
+                <meta charSet="utf-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="description" content="Personal website" />
+                <title>Inicio</title>
+            </Head>
+            <Header />
+            <Main>{props.children}</Main>
+            <ButtonGoToUp href='/#top' className={active}>
+                <FaArrowUp />
+            </ButtonGoToUp>
+            <Footer />
+        </>
+    )
+}
