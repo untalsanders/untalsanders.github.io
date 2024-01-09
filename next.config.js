788dc41da7a2a455d414fd18427f07c06cadf36c
Sanders Gutiérrez <ing.sanders@gmail.com>
2024-01-09 02:10:20 -0300

    fix: add output export
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withMDX = require('@next/mdx')()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     pageExtensions: ['js', 'jsx', 'mdx'],
     reactStrictMode: true,
     compiler: {
