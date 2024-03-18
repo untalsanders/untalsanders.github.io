f33a46fe396dba4b8ea73ea7ca064a1f4e42f0ff
Sanders Gutiérrez <ing.sanders@gmail.com>
2024-03-18 05:59:22 -0300

    chore: home page is works
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from '@/routes'
-import './styles.css'
+import '@assets/css/normalize.css'
+import '@styles/global.css'
 
 const root = createRoot(document.querySelector('#root'))
 root.render(
