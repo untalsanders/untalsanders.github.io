01dac905cab9225211a28e54ae0c0a7a8130d6dc
Sanders Gutiérrez <ing.sanders@gmail.com>
2024-03-22 00:16:26 -0300

    fix: attributes jsx compatibles

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -24,10 +24,10 @@ export const HomePage = () => (
                         <svg
                             stroke="currentColor"
                             fill="currentColor"
-                            stroke-width="0"
+                            strokeWidth="0"
                             version="1"
                             viewBox="0 0 48 48"
-                            enable-background="new 0 0 48 48"
+                            enableBackground="new 0 0 48 48"
                             height={30}
                             width={30}
                             xmlns="http://www.w3.org/2000/svg">
