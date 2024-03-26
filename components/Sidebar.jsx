d4b944e55839793c2ee7831fb1f1306447e0ab4e
Sanders Gutiérrez <ing.sanders@gmail.com>
2024-03-25 22:31:18 -0300

    remove files unnecessary

--- components/Sidebar.jsx
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-import styled from 'styled-components'
-
-const SidebarContainer = styled.aside``
-
-export function Sidebar({ children }) {
-    return <SidebarContainer>{children}</SidebarContainer>
-}
--- pages/404.jsx
+++
@@ -1,9 +0,0 @@
-import { PageLayout } from "@/components/index.js";
-
-export default function Custom404() {
-    return (
-        <PageLayout>
-            <h1>404 - Page Not Found .....</h1>
-        </PageLayout>
-    )
-}
