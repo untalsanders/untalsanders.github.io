a8c245c9cf72e83efe7c54666a1d0421ca1ba2fe
Sanders Gutiérrez <ing.sanders@gmail.com>
2024-05-01 21:10:51 -0300

    chore: Typescript support added
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
+import Head from '@/partials/Head'
+
 export default function RootLayout({
     children,
-}) {
+}: Readonly<{
+    children: React.ReactNode
+}>) {
     return (
         <html lang="en">
+            <Head />
             <body>{children}</body>
         </html>
     )
---
+++ src/partials/Head.tsx
@@ -0,0 +1,13 @@
+import 'normalize.css'
+import '@/assets/css/globals.css'
+
+export default function Head() {
+    return (
+        <head>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+            <title>Sanders Gutiérrez</title>
+        </head>
+    )
+}
