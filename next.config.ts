ec6eff0b84ac67256bd890a9cdd226c6db82fc2c
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-04-21 07:11:29 -0300

    chore: add MDX support and update dependencies in configuration files

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from 'next'
+import createMDX from '@next/mdx'
 
 const nextConfig: NextConfig = {
     output: 'export',
     distDir: 'dist',
+    pageExtensions: ['jsx', 'js', 'ts', 'tsx', 'mdx', 'md',],
 }
 
-export default nextConfig
+const withMDX = createMDX({
+    // Add your MDX options here
+})
+
+export default withMDX(nextConfig)
