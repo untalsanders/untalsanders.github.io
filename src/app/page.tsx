7b35e0ce59fc102883a9e660c950b35fecb307a3
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-06-30 18:51:22 -0300

    refactor: restructure HomePage component layout for including contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import Contact from '@/components/Contact'
 import Hero from "@/components/Hero"
 import styles from '@/styles/HomePage.module.css'
 
 const HomePage = () => {
     return (
-        <div className={`${styles.Hero} container`}>
+        <>
+            <div className={styles.hero}>
             <Hero />
         </div>
+            <Contact />
+        </>
     )
 }
 
