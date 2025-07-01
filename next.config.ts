18502f70353ef45798ef3fa7fc3ed1355d041985
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-06-30 23:05:42 -0300

    fix: remove distDir configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next'
 import createMDX from '@next/mdx'
 
 const nextConfig: NextConfig = {
-    distDir: 'dist',
     pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 }
 
