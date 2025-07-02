489bddf7d959a5d871ddb94d8e61acdda1756cd1
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-07-02 03:51:48 -0300

    feat: update description of services

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -11,46 +11,43 @@ export default function Services() {
                         Services
                     </h2>
                     <p>
-                        Have a project in mind or just want to chat about code? Drop me a message, and let&apos;s make
-                        things happen!
+                        I offer a different range of services related to development, including working on complete projects from their inception until deployment, or the maintenance of existing codebases.
                     </p>
                 </header>
-                <section>
-                    <div className={`${styles.ServicePage__Service_items} container`}>
-                        <article>
-                            <span>
-                                <FaCode />
-                            </span>
-                            <h3>Software Development</h3>
-                            <p>
-                                Get an optimized design aligned with the visual identity of your brand, planned from
-                                usability and become more visible to your audiences through a friendly design optimized
-                                for search engines.
-                            </p>
-                        </article>
-                        <article>
-                            <span>
-                                <FaMobileAlt />
-                            </span>
-                            <h3>Mobile Apps</h3>
-                            <p>
-                                Align current mobile trends, to improve your processes and increase the level of
-                                accessibility and visibility of your customers, allowing you to build loyalty with your
-                                target audience, since they can be in front of your business at all times.
-                            </p>
-                        </article>
-                        <article>
-                            <span>
-                                <FaScrewdriverWrench />
-                            </span>
-                            <h3>Consultancy</h3>
-                            <p>
-                                Ensure the perfect functioning of your company, through a specialized computer
-                                consultancy, which ranges from the analysis of requirements to the analysis and
-                                corrective and preventive maintenance of your computer applications.
-                            </p>
-                        </article>
-                    </div>
+                <section className={`${styles.ServicePage__Service_items} container`}>
+                    <article>
+                        <span>
+                            <FaCode />
+                        </span>
+                        <h3>Software Development</h3>
+                        <p>
+                            Get an optimized design aligned with the visual identity of your brand, planned from
+                            usability and become more visible to your audiences through a friendly design optimized
+                            for search engines.
+                        </p>
+                    </article>
+                    <article>
+                        <span>
+                            <FaMobileAlt />
+                        </span>
+                        <h3>Mobile Apps</h3>
+                        <p>
+                            Align current mobile trends, to improve your processes and increase the level of
+                            accessibility and visibility of your customers, allowing you to build loyalty with your
+                            target audience, since they can be in front of your business at all times.
+                        </p>
+                    </article>
+                    <article>
+                        <span>
+                            <FaScrewdriverWrench />
+                        </span>
+                        <h3>Consultancy</h3>
+                        <p>
+                            Ensure the perfect functioning of your company, through a specialized computer
+                            consultancy, which ranges from the analysis of requirements to the analysis and
+                            corrective and preventive maintenance of your computer applications.
+                        </p>
+                    </article>
                 </section>
             </div>
         </div>
